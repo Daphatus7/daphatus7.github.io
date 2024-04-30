655ad9f8ad003d8c7867897d092480b530961647
peiyuw2 <peiyuw08@gmail.com>
2024-04-30 11:39:57 +1000

    add responsiveness

---
+++ scripts/js/product-display-loader.js
@@ -0,0 +1,75 @@
+const products = [
+    {
+        name: "Zola Flush Light",
+        imagePath: "images/flush-image.png",
+        price: "300",
+    },    
+    {
+        name: "Zola Flush Light",
+        imagePath: "images/flush-image.png",
+        price: "300",
+    },    
+    {
+        name: "Zola Flush Light",
+        imagePath: "images/flush-image.png",
+        price: "300",
+    },    
+    {
+        name: "Zola Flush Light",
+        imagePath: "images/flush-image.png",
+        price: "300",
+    },
+    {
+        name: "Zola Flush Light",
+        imagePath: "images/flush-image.png",
+        price: "300",
+    },
+    {
+        name: "Zola Flush Light",
+        imagePath: "images/flush-image.png",
+        price: "300",
+    },
+    {
+        name: "Zola Flush Light",
+        imagePath: "images/flush-image.png",
+        price: "300",
+    },
+    {
+        name: "Zola Flush Light",
+        imagePath: "images/flush-image.png",
+        price: "300",
+    },
+    {
+        name: "Zola Flush Light",
+        imagePath: "images/flush-image.png",
+        price: "300",
+    },
+    {
+        name: "Zola Flush Light",
+        imagePath: "images/flush-image.png",
+        price: "300",
+    },
+    
+];
+
+function displayProducts(products) {
+    let product_list = document.getElementsByClassName('product-list-items')[0];
+    console.log("sds");
+    fetch('product-item-display-template.html')
+        .then(response => response.text())
+        .then(template => {
+            products.forEach(product => {
+                let productDisplay = document.createElement('div');
+                productDisplay.innerHTML = template
+                productDisplay.querySelector(".product-name-text")
+                    .innerHTML = product.name;
+                productDisplay.querySelector(".price")
+                    .innerHTML = product.price;
+                productDisplay.querySelector(".product-item-display-image")
+                    .src = product.imagePath;
+                product_list.appendChild(productDisplay);
+            })
+        })
+}
+
+addEventListener('DOMContentLoaded', () => displayProducts(products));
