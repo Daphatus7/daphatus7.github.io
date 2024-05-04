caafd984dcd0c1856f0e805801c05a14bc6b7f9f
peiyuw2 <peiyuw08@gmail.com>
2024-05-04 15:08:37 +1000

    remove useless components

--- scripts/js/checkout-order-summary.js
+++
@@ -1,5 +0,0 @@
-fetch('page-components/order-summary-items.html')
-    .then(response => response.text())
-    .then(html => {
-        document.getElementById('checkout-order-items-placeholder').innerHTML = html;
-    })
