48e9bbd98bbd4cee65bff92961a0cc0243a87073
peiyuw2 <peiyuw08@gmail.com>
2024-05-11 18:58:39 +1000

    fix the menu

--- scripts/js/navigation-bar.js
+++ scripts/js/navigation-bar.js
@@ -1,7 +1,9 @@
 function showMenu() {
     document.getElementById('mobile--menu-container').style.display = 'flex';
+    document.getElementById('navigation__bar_mobile').style.display = 'none';
 }
 
 function hideMenu() {
     document.getElementById('mobile--menu-container').style.display = 'none';
+    document.getElementById('navigation__bar_mobile').style.display = 'flex';
 }
